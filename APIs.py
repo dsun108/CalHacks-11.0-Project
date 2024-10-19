90cadee04a56efcdce2ce8145ff0203598f816a5
Randy <randyjbui888@gmail.com>
2024-10-19 13:25:06 -0700

    Requirements.txt

--- APIs.py
+++ APIs.py
@@ -1,1 +1,13 @@
 from flask import Flask, jsonify
+import os
+from flask_restful import Resource, Api
+import google.generativeai as genai
+import Keys
+app = Flask(__name__)
+genai.configure(api_key=os.environ['API_key'])
+model = genai.GenerativeModel("gemini-1.5-flash")
+
+@app.route("/")
+
+def prompt_gene():
+    return "<p>"
