49c7df6adaddb6197c7e9cc0a8fc6cdfe65a6b1f
Randy <randyjbui888@gmail.com>
2024-10-19 14:40:13 -0700

    start of API Calls

--- wpg/ContentView.swift
+++ wpg/ContentView.swift
@@ -7,6 +7,20 @@
 
 import SwiftUI
 
+// class geneaiPrompt: ObservableObject{
+//     let urlstring: String = "ttps://http://127.0.0.1:5000"
+//     func get(){
+//         let  Endpoint: String = "how to make a good soup?" //Random query, replace this with text input
+//         guard let url = URL(String: urlstring  + EndPoint ) else{/*print an error in chatbox here*/return}
+//     }
+
+//     var request = URLREQUEST(url: url)
+//     request.httpMethod = "Get"
+
+//     let task = URLSession.shared.dataTask(with:request) {data,response, error in if let error = error{return}}
+
+//     }
+
 struct ContentView: View {
     var body: some View {
         TabView{
