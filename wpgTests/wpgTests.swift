7c6d35773b89934094ec81efa0d0bf3b70d9c22f
David Sun <davidsun0709@gmail.com>
2024-10-19 12:33:33 -0700

    Initial Commit

---
+++ wpgTests/wpgTests.swift
@@ -0,0 +1,16 @@
+//
+//  wpgTests.swift
+//  wpgTests
+//
+//  Created by David Sun on 10/19/24.
+//
+
+import Testing
+
+struct wpgTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
