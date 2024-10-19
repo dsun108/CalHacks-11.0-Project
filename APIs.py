aa81e0d05629826fc5238de1cf8c3050e8e6b84f
Randy <randyjbui888@gmail.com>
2024-10-19 13:43:24 -0700

    Finished Google Integration
    Signed-off-by: Randy <randyjbui888@gmail.com>

--- APIs.py
+++ APIs.py
@@ -4,10 +4,15 @@ from flask_restful import Resource, Api
 import google.generativeai as genai
 import Keys
 app = Flask(__name__)
+api = Api(app)  
 genai.configure(api_key=os.environ['API_key'])
 model = genai.GenerativeModel("gemini-1.5-flash")
+if True:
+    app.run(debug=True)
 
-@app.route("/")
 
-def prompt_gene():
-    return "<p>"
+class getPrompt(Resource):
+    def get(self, query):
+        response  = model.generate_content(query)
+        return {"prompt":query, "responses":response.text}
+api.add_resource(getPrompt, '/<string:query>')
