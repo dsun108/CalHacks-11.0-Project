c1368573e772f9375d9c784257b15ab0d3e1ff6f
edwin1in <el086479@gmail.com>
2024-10-19 14:47:08 -0700

    test

--- hello.py
+++ hello.py
@@ -5,6 +5,5 @@ app = Flask(__name__)
 test = ['hi', 't']
 @app.route("/", methods=['GET','POST'])
 def hello_world():
-    print(request.method)
-    if request.method  == 'GET':
+    if request.method == 'GET':
         return jsonify(test)
