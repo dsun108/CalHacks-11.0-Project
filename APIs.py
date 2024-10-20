435d4975fe12ab300f486e6b40415cab6ece66bc
Randy <randyjbui888@gmail.com>
2024-10-19 18:27:03 -0700

    stash
    Signed-off-by: Randy <randyjbui888@gmail.com>

--- APIs.py
+++ APIs.py
@@ -14,7 +14,7 @@ if True:
 
 class getPrompt(Resource):
     def get(self, query):
-        
+
         response  = model.generate_content(query)
         return {"prompt":query, "responses":response.text}
 
@@ -26,24 +26,26 @@ class getNutrient(Resource):
         response  = requests.get(url)
         return {'Brand': response.json()['foods'][0]['brandName'], 'Protein':response.json()['foods'][0]['foodNutrients'][0]['value'], 'ServingSize':response.json()['foods'][0]['servingSize'], 'kCal':response.json()['foods'][0]['foodNutrients'][3]['value']}
 class workout(Resource):
-    def get(self,Exercise):
+    def get(self,prof,days,muscle,goal):
         muscles =["abdominals","adductors","biceps","calves","chest","forearms","glutes","hamstrings","lats","lower_back","middle_back","neck","quadriceps","traps","triceps"]
         style = ["cardio","olympic_weightlifting","plyometrics","powerlifting","strength","stretching","strongman"]
         difficulty =  ["beginner","intermediate","expert"]
         url = 'https://api.api-ninjas.com/v1/'
-        if Exercise in muscles:
-            url = url+'exercises?muscles='+Exercise
-        if Exercise in style:
-            url = url+'exercises?type='+Exercise
-        if Exercise in difficulty:
-            url = url+'exercises?difficulty='+Exercise
-        result = requests.get(url, headers={'X-Api-Key': os.environ['ninja']})
-        dict = {}
-        for i in range(5):
-            dict['name'+str(i)] = result.json()[i]['name']
-            dict['howto'+str(i)] = result.json()[i]['instructions']
+        
+        # if Exercise in muscles:
+        #     url = url+'exercises?muscles='+Exercise
+        # if Exercise in style:
+        #     url = url+'exercises?type='+Exercise
+        # if Exercise in difficulty:
+        #     url = url+'exercises?difficulty='+Exercise
+        # result = requests.get(url, headers={'X-Api-Key': os.environ['ninja']})
+        # dict = {}
+        # for i in range(5):
+        #     dict['name'+str(i)] = result.json()[i]['name']
+        #     dict['howto'+str(i)] = result.json()[i]['instructions']
 
-        return dict
+        # return dict
 api.add_resource(getPrompt, '/google/<string:query>')
 api.add_resource(getNutrient, '/food/<string:food>')
-api.add_resource(workout, '/workout/<string:Exercise>')
+api.add_resource(workout, '/workout/<string:prof>/<string:day>/<string:muscle>/<string:goal>')
+
