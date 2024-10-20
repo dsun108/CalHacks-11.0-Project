20b9a480c9f16e7aae9d950e3d2b3eec19f48f12
Randy <randyjbui888@gmail.com>
2024-10-19 22:18:39 -0700

    10:18 pm i had 4 hours of sleep

--- APIs.py
+++ APIs.py
@@ -3,20 +3,17 @@ import os
 from flask_restful import Resource, Api
 import google.generativeai as genai
 import Keys
+import random
 import requests
+import time
 app = Flask(__name__)
-api = Api(app)  
+api = Api(app)
+random.seed(time.time())
 genai.configure(api_key=os.environ['API_Key'])
 model = genai.GenerativeModel("gemini-1.5-flash")
 
-@app.route("/userData",methods=["POST"])
-def getter():
-	print(request.data)
-	return {"lol":"lol"}
-
 class getPrompt(Resource):
     def get(self, query):
-        
         response  = model.generate_content(query)
         return {"prompt":query, "responses":response.text}
 
@@ -33,19 +30,107 @@ class workout(Resource):
         style = ["cardio","olympic_weightlifting","plyometrics","powerlifting","strength","stretching","strongman"]
         difficulty =  ["beginner","intermediate","expert"]
         url = 'https://api.api-ninjas.com/v1/'
+        toggle = True
         if Exercise in muscles:
-            url = url+'exercises?muscles='+Exercise
+            url = url+'exercises?muscle='+Exercise
         if Exercise in style:
             url = url+'exercises?type='+Exercise
         if Exercise in difficulty:
             url = url+'exercises?difficulty='+Exercise
-        result = requests.get(url, headers={'X-Api-Key': os.environ['ninja']})
-        dict = {}
-        for i in range(5):
-            dict['name'+str(i)] = result.json()[i]['name']
-            dict['howto'+str(i)] = result.json()[i]['instructions']
+        if url == 'https://api.api-ninjas.com/v1/':
+            url = url+'exercises?name='+Exercise
+            toggle = False
 
+        result = requests.get(url, headers={'X-Api-Key': os.environ['ninja']})
+        
+        dict = {}
+        dict['toggle'] = True
+        if(toggle):
+            for i in range(5):
+                dict['name'+str(i)] = result.json()[i]['name']
+                dict['howto'+str(i)] = result.json()[i]['instructions']
+        else:
+            for i in range(5):
+                dict['name'+str(i)] = result.json()[0]['name']
+                dict['muscle'+str(i)] = result.json()[0]['muscle']
+            dict['toggle'] = False
         return dict
 api.add_resource(getPrompt, '/google/<string:query>')
 api.add_resource(getNutrient, '/food/<string:food>')
 api.add_resource(workout, '/workout/<string:Exercise>')
+
+@app.route("/userData",methods=["POST"])
+def getter():
+    data = request.get_json(force=True)
+
+    type = data['goals']
+    exp = data['exp']
+    time = data['time']
+    dict = {'Day1':'Rest', 'Day2':'Rest', 'Day3':'Rest', 'Day4':'Rest', 'Day5':'Rest'}
+    obj = workout.get(workout,'biceps')
+
+
+    lst2= []
+    for i in range(2):
+        lst = []
+        while(True):
+            j = random.randrange(0,5)
+            if j not in lst:
+                lst += [j]
+                break
+        lst2 += [obj['name'+str(j)]]
+
+    obj = workout.get(workout,'middle_back')
+    lst3= []
+    for i in range(3):
+        lst = []
+        while(True):
+            j = random.randrange(0,5)
+            if j not in lst:
+                lst += [j]
+                break
+        lst3 += [obj['name'+str(j)]]
+    dict['Day1'] = lst2+lst3
+    if(int(time) >= 4):
+        dict['Day4'] = lst2+lst3
+
+
+    obj = workout.get(workout,'chest')
+
+    lst2= []
+    for i in range(5):
+        lst = []
+        while(True):
+            j = random.randrange(0,5)
+            if j not in lst:
+                lst += [j]
+                break
+        lst2 += [obj['name'+str(j)]]
+
+    dict['Day2'] = lst2
+    if(int(time) >= 5):
+        dict['Day5'] = lst2
+    obj = workout.get(workout,'quadriceps')
+    for i in range(5):
+        lst = []
+        while(True):
+            j = random.randrange(0,5)
+            if j not in lst:
+                lst += [j]
+                break
+        lst2 += [obj['name'+str(j)]]
+        
+    dict['Day3'] = lst2
+    return dict
+
+@app.route("/reverse",methods=["POST"])
+def getter2():
+    data = request.get_json(force=True)
+    exercise = data['exercise']
+    obj = workout.get(workout,exercise)
+    dict = {}
+    for i in range(5):
+        dict['name'+str(i)] = obj['name'+str(i)]
+        dict['muscle'+str(i)] = obj['muscle'+str(i)]
+    print(dict)
+    return dict
--- Test.py
+++ Test.py
@@ -2,3 +2,6 @@ import requests
 import Keys
 import os
 import webbrowser
+from flask import jsonify
+
+request = requests.post("http://127.0.0.1:5000/reverse", json=({'exercise':'shoulder press'}))
